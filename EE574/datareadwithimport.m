a4ce975e5076a75541f4b951930676d336eeb505
Erencan Duymaz <erencanduymaz@gmail.com>
2017-01-04 21:48:50 +0300

    Data is read successfully.

--- EE574/datareadwithimport.m
+++ EE574/datareadwithimport.m
@@ -6,7 +6,43 @@ close all;
 clc;
 
 ieee_data = importdata( 'ieee_cdf.dat');
-ieee_data_matrix = ieee_data.data;
+busdata = ieee_data.data;
+ieee_data_matrix_size = size(busdata);
+
+busnumber = ieee_data_matrix_size(1,1);
 
 measurementdata = dlmread('measure.dat');
+
+% headerlinesOut = 34;
+
+filename = 'ieee_cdf.dat';
+headerlinesIn = busnumber+4;
+delimiterIn = ' ';
+A = importdata(filename,delimiterIn,headerlinesIn)
+B = A.data;
+C = size(B);
+D = C(1,1);
+B(D,:)=[];
+branchdata = B;
+
+% All data is read. Namely, busdata, measurementdata and branchdata. 
+
+% Now, the taken measurement numbers should be read.
+
+n_v = measurementdata(1,1); % First line indicates the voltage measurement number. 
+measurementdata(1,:) = []; % Disregard this line. Voltage measurement number is known anymore.
+
+n_pi = measurementdata(n_v+1,1); % Number of power injection measurement is found. No need to find n_qi since it is equal to n_pi.
+
+measurementdata(n_v+1,:) = []; % Disregard this line. Power injection measurement number is known anymore.
+measurementdata(n_v+n_pi+1,:) = []; % Disregard this line. Reactive power injection measurement number is known anymore.
+
+n_pf = measurementdata(n_v+2*n_pi+1,1); % Number of power flow measurement is found. No need to find n_qf since it is equal to n_pf.
+
+measurementdata(n_v+2*n_pi+1,:) = []; % Disregard this line. Power flow measurement number is known anymore.
+measurementdata(n_v+2*n_pi+n_pf+1,:) = []; % Disregard this line. Reactive power flow measurement number is known anymore.
+
+n_c = measurementdata(n_v+2*n_pi+2*n_pf+1,1);
+measurementdata(n_v+2*n_pi+2*n_pf+1,:) = [];
+measurementdata(n_v+2*n_pi+2*n_pf+n_c+1,:) = [];
 
