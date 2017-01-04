45fbfca26aa7de02075ec36f2861041d36ab7901
Erencan Duymaz <erencanduymaz@gmail.com>
2017-01-04 23:52:59 +0300

    Jacobian Computation is started from injection derivatives

--- EE574/datareadwithimport.m
+++ EE574/datareadwithimport.m
@@ -71,10 +71,31 @@ end
     
 % Initiate the Jacobian matrix  whose size is (measurementnumber)X(2*busnumber+tapnumber-1)
 
+H = zeros(measurementnumber,2*busnumber-1+tapnumber);
 
+%First part of the Jacobian matrix is derivatives of the measurement
+%voltages with respect to state vectors 
 
+for count = 1:n_v
+    H(count,measurementdata(count,1))= measurementdata(count,1);
+end
 
+% Second part of the Jacobian is derivatives of the power injection
+% measurements with respect to state vectors 
+% delPij/Vi = Vi^2(gij)-Vj(gij cos theta(ij) + bij sin theta ij )
 
+% Note that theta1 is assumed 0;
+for count = 1:branchnumber
+    if x(busnumber+branchdata(count,1),1) == 1
+    thetaij = -x(busnumber+branchdata(count,2),1);
+    else 
+    thetaij = x(busnumber+branchdata(count,1),1)-x(busnumber+branchdata(count,2),1)
+    end
+    H(n_v+count,branchdata(count,1)) = ((2*(x(branchdata(count,1),1))))*...
+        (inv(branchdata(count,1)))-(x(branchdata(count,2)))*...
+        ((inv(branchdata(count,7)))*cos(thetaij)+...
+        (((inv(branchdata(count,8)))*sin(thetaij))))
+end 
 
 
 
