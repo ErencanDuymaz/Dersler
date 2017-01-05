c6028bda2091d85b1a397e2bb4cfd6980e2a1f8b
Erencan Duymaz <erencanduymaz@gmail.com>
2017-01-05 12:46:14 +0300

    Active power injection derivatives added.

--- EE574/datareadwithimport.m
+++ EE574/datareadwithimport.m
@@ -26,6 +26,17 @@ B(D,:)=[];
 branchdata = B;
 sizebranch = size(branchdata);
 branchnumber = sizebranch(1,1);
+
+% Line impedence is given by R+jX. Therefore we should find G+jB. 
+% We will write down G and B values to the 10th and 11th column.
+% Line charging B should also be divided by two. It is written in 12nd
+% column.
+
+for count = 1:branchnumber
+    branchdata(count,10) = real(inv(branchdata(count,7)+i*branchdata(count,8)));
+    branchdata(count,11) = imag(inv(branchdata(count,7)+i*branchdata(count,8)));
+    branchdata(count,12) = branchdata(count,9)/2;
+end
 % All data is read. Namely, busdata, measurementdata and branchdata. 
 
 % Now, the taken measurement numbers should be read.
@@ -80,7 +91,7 @@ for count = 1:n_v
     H(count,measurementdata(count,1))= measurementdata(count,1);
 end
 
-% Second part of the Jacobian is derivatives of the power injection
+% Second part of the Jacobian is derivatives of the power flow
 % measurements with respect to state vectors 
 % delPij/Vi = Vi^2(gij)-Vj(gij cos theta(ij) + bij sin theta ij )
 
@@ -89,12 +100,35 @@ for count = 1:branchnumber
     if x(busnumber+branchdata(count,1),1) == 1
     thetaij = -x(busnumber+branchdata(count,2),1);
     else 
-    thetaij = x(busnumber+branchdata(count,1),1)-x(busnumber+branchdata(count,2),1)
+    thetaij = x(busnumber+branchdata(count,1),1)-x(busnumber+branchdata(count,2),1);
     end
-    H(n_v+count,branchdata(count,1)) = ((2*(x(branchdata(count,1),1))))*...
-        (inv(branchdata(count,1)))-(x(branchdata(count,2)))*...
-        ((inv(branchdata(count,7)))*cos(thetaij)+...
-        (((inv(branchdata(count,8)))*sin(thetaij))))
+    
+    % delPij/Vi = Vi^2(gij)-Vj(gij cos theta(ij) + bij sin theta ij )
+
+    H(n_v+2*n_pi+count,branchdata(count,1)) = (2*(x(branchdata(count,1),1)))*...
+        (branchdata(count,10))-((x(branchdata(count,2),1))*...
+        branchdata(count,10)*cos(thetaij)+...
+        (branchdata(count,11))*sin(thetaij));
+    
+    % delPij/Vj = -Vi(gij cos theta(ij) + bij sin theta ij )
+
+    H(n_v+2*n_pi+count,branchdata(count,2)) = -(x(branchdata(count,1),1))*...
+        (branchdata(count,10)*cos(thetaij)+...
+        (branchdata(count,11))*sin(thetaij));
+    
+    % delPij/thetai = Vi(gij sin theta(ij) - bij cos theta ij )
+    
+    if branchdata(count,1) ~= 1;
+    H(n_v+count+2*n_pi,busnumber+branchdata(count,1)-1) = (x(branchdata(count,1),1))*(x(branchdata(count,2),1))*...
+        (branchdata(count,10)*sin(thetaij)-...
+        (branchdata(count,11))*cos(thetaij));
+    end
+    % delPij/thetaj = -ViVj(gij sin theta(ij) - bij cos theta ij )
+    if branchdata(count,2) ~= 1;
+    H(n_v+count+2*n_pi,busnumber+branchdata(count,2)-1) = -(x(branchdata(count,1),1))*(x(branchdata(count,2),1))*...
+        (branchdata(count,10)*sin(thetaij)-...
+        (branchdata(count,11))*cos(thetaij));
+    end
 end 
 
 
